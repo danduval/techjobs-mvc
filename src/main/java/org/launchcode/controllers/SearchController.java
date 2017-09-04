b62ac0c2d26eb5464beae4af03584cdad3aaf889
Dan Duval <duval.dan@gmail.com>
2017-09-04 03:13:35 -0700

    Nearly finished assignment - for some reason I can't search the "core competency (skill)" column, and my search results are displaying at "/search/results" instead of just "/search", but other than that I think this is pretty much there.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,25 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    // From the instructions: "Add another results handler method to SearchController, overloading the existing method."
+    // What?  I don't see an existing method called "results."  Anyway, here we go.
+
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String results(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+
+        ArrayList<HashMap<String, String>> Jobs = new ArrayList<>();
+
+        if (!searchType.equals("all")) {
+            Jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        } else if (searchType.equals("all")) {
+            Jobs = JobData.findByValue(searchTerm);
+        }
+
+        model.addAttribute("Jobs", Jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+
+        return "search";
+    }
 
 }
